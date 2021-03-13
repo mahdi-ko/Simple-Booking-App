1726c502ff058f4cc16c279072c0cd30c59f1c51
mahdi-ko <mahdi-ko@users.noreply.github.com>
2021-03-13 04:23:21 +0200

    moved header widget to common widgets

--- lib/common/common_widgets.dart
+++ lib/common/common_widgets.dart
@@ -5,3 +5,22 @@ ButtonStyle orangeButtonStyle(ThemeData? theme) {
     return theme!.accentColor;
   }));
 }
+class HeaderText extends StatelessWidget {
+  final String text;
+  final ThemeData theme;
+  const HeaderText(this.text, this.theme);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: Text(text,
+          style: theme.textTheme.headline5?.copyWith(shadows: [
+            Shadow(
+              color: Colors.grey.shade400,
+              offset: Offset(2, 1.5),
+            )
+          ])),
+    );
+  }
+}
